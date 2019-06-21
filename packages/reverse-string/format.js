e57acb01809b16dc9f1c238fca7310fd0c57c706
Hunter Parcells <hparcells@icloud.com>
2019-06-21 12:08:43 -0400

    Added a Return

--- packages/reverse-string/format.js
+++ packages/reverse-string/format.js
@@ -2,6 +2,7 @@
  * Formats a string by substituting variables.
  * @param {String} format The string to format.
  * @param  {...any} variables Any variables to substitute in the string.
+ * @returns {String} A formatted string with variables substituted in.
  */
 function format(format, ...variables) {
   return format.replace(/\$[0-9]+/g, (x) => String(variables[parseInt(x.substring(1)) - 1]));
