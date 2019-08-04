351a7c8f332eb3e13b3f928127643ef0c721f2ff
Dave Caruso <dave@davecode.me>
2019-08-04 12:40:53 -0400

    [Array/remove] Removes only the first item

--- packages/reverse-array/remove.ts
+++ packages/reverse-array/remove.ts
@@ -2,5 +2,6 @@
  * Removes an element from an array by it's value.
  */
 export function remove<T>(array: T[], value: T): T[] {
-  return array.filter((x) => x !== value);
+  let loop = true;
+  return array.filter((x) => loop && (x !== value || (loop = false)));
 }
