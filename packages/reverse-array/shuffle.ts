f8e31054dc40daf65fa2f142a106f8f0b117dcd4
Dave Caruso <dave@davecode.me>
2019-08-06 23:33:40 -0400

    New Shuffle

--- packages/reverse-array/shuffle.ts
+++ packages/reverse-array/shuffle.ts
@@ -5,13 +5,24 @@ import { remove } from "./remove";
  * Shuffles an array and returns it.
  */
 export function shuffle<T>(array: T[]): T[] {
-  const newArray: Array<T> = [];
-  let shuffleArray = array.concat();
+  var newArray: T[] = [];
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
-  while (shuffleArray.length > 0) {
-    const item = randomOf(shuffleArray);
-    newArray.push(item);
-    shuffleArray = remove(shuffleArray, item);
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    if (newArray.hasOwnProperty(currentIndex)) {
+      temporaryValue = newArray[currentIndex];
+    } else {
+      temporaryValue = array[currentIndex];
+    }
+    newArray[currentIndex] = array[randomIndex];
+    newArray[randomIndex] = temporaryValue;
   }
 
   return newArray;
