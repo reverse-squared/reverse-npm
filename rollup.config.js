f74bb01e3a3a79f26426e95aa79deaebc0a785ba
dave caruso <dave@davecode.me>
2019-12-26 19:04:57 -0500

    Fix build

--- rollup.config.js
+++ rollup.config.js
@@ -3,26 +3,31 @@ import typescript from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import fs from 'fs';
+import path from 'path';
 
-export default fs.readdirSync('./packages').filter((x) => x.startsWith('reverse-')).map((name) => ({
-  // this file imports logic and gives a node.js prompt
-  input: `./packages/${name}/index.ts`,
-  plugins: [
-    // add typescript compiling
-    typescript({
-      rollupCommonJSResolveHack: true,
-    }),
-    //
-    commonjs(),
-    // fix the import resolver because by default it doesn't work.
-    resolve({
-      extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
-      preferBuiltins: true,
-    }),
-  ],
-  // output the file
-  output: {
-    file: `./packages/${name}/index.js`,
-    format: 'cjs',
-  },
-}));
+export default fs
+  .readdirSync('./packages')
+  .filter((x) => x.startsWith('reverse-'))
+  .map((name) => ({
+    // this file imports logic and gives a node.js prompt
+    input: `./packages/${name}/index.ts`,
+    plugins: [
+      // add typescript compiling
+      typescript({
+        rollupCommonJSResolveHack: true,
+        useTsconfigDeclarationDir: true,
+      }),
+      //
+      commonjs(),
+      // fix the import resolver because by default it doesn't work.
+      resolve({
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+        preferBuiltins: true,
+      }),
+    ],
+    // output the file
+    output: {
+      file: `./packages/${name}/index.js`,
+      format: 'cjs',
+    },
+  }));
